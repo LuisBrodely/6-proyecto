1f5c1f4201401fe9c55d17efc76869b3439d5983
LuisBrodely <luisbrodely2@gmail.com>
2023-06-19 03:57:39 -0600

    first commit

--- sections/Loading.jsx
+++ sections/Loading.jsx
@@ -120,6 +120,7 @@ export default function Loading() {
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
+                  target='https://github.com/LuisBrodely/6-proyecto'
                   className="rounded-md bg-[#10ac97] px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   GitHub
